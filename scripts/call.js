1f8db84873efa7cfe2104bf22907a0b0ae3a9e5f
grandlavogue <rhea.goldensky22@gmail.com>
2024-11-30 13:10:30 +0700

    Update call.js

--- scripts/call.js
+++ scripts/call.js
@@ -4,11 +4,11 @@ function copyKakaoID() {
 
     // Copy to clipboard
     navigator.clipboard.writeText(kakaoID).then(() => {
-      // Show a confirmation message
-      const message = document.getElementById("copyMessage");
-      message.style.display = "block";
-      setTimeout(() => {
+    // Show a confirmation message
+    const message = document.getElementById("copyMessage");
+    message.style.display = "block";
+    setTimeout(() => {
         message.style.display = "none";
-      }, 2000); // Hide after 2 seconds
+    }, 2000); // Hide after 2 seconds
     });
-  }
+}
